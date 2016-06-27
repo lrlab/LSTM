214bcad336fbfca8701887b3c602203b6c52ff36
tnksr <sora@oku-dhcp03.lrlab>
2016-06-27 17:14:22 +0900

    vocabularyの修正

--- seq2seq.py
+++ seq2seq.py
@@ -2,7 +2,7 @@
 # coding: UTF-8
 import argparse
 import numpy as np
-
+import pickle
 import chainer
 import chainer.functions as F
 from chainer import optimizers, serializers
@@ -17,7 +17,7 @@ def parse_args():
     parser.add_argument("source", type=str)
     parser.add_argument("target", type=str)
     parser.add_argument("mode", type=str, help="train mode or test mode")
-    parser.add_argument("--epoch", dest="epoch", type=int, default=10)
+    parser.add_argument("--epoch", dest="epoch", type=int, default=30)
     parser.add_argument("--embed_size", dest="embed_size", type=int, default=100)
     parser.add_argument("--hidden_size", dest="hidden_size", type=int, default=200,
                         help="the number of cells at hidden layer")
@@ -27,8 +27,14 @@ def parse_args():
 def text_generator(file_name):
     with open(file_name, "r") as f:
         for line in f:
-            print(line)
             yield line
+
+
+def load_train_vocabulary():
+    with open("train_data", "r") as f:
+        train_source = str(next(f)).strip("\n")
+        train_target = str(next(f))
+    return train_source, train_target
 
 
 class EncoderDecoder(chainer.Chain):
@@ -36,7 +42,6 @@ class EncoderDecoder(chainer.Chain):
 
         self.src_vocabulary = Vocabulary()
         self.src_vocabulary.make_dictionary(src_file)
-
         self.trg_vocabulary = Vocabulary()
         self.trg_vocabulary.make_dictionary(trg_file)
 
@@ -51,10 +56,10 @@ class EncoderDecoder(chainer.Chain):
             w_ep=F.Linear(self.embed_size, self.hidden_size*4),
             w_pp=F.Linear(self.hidden_size, self.hidden_size*4),
             # decoder
-            w_ye=F.EmbedID(self.trg_size, self.embed_size),
-            w_eq=F.Linear(self.embed_size, self.hidden_size*4),
+            w_ey=F.EmbedID(self.trg_size, self.embed_size),
+            w_qe=F.Linear(self.embed_size, self.hidden_size*4),
             w_qq=F.Linear(self.hidden_size, self.hidden_size*4),
-            w_qy=F.Linear(self.hidden_size, self.trg_size),
+            w_yq=F.Linear(self.hidden_size, self.trg_size),
         )
 
 
@@ -81,12 +86,13 @@ def train(args, model):
     # save
     serializers.save_npz("model", model)
     # TODO : save args
-    #serializers.save_npz("state", opt)
+    # model.embed_size, model.hidden_size, vocabulary
+    # serializers.save_npz("state", opt)
 
 
 def test(args, model):
-    #serializers.load_npz("model", model)
-    #opt = serializers.load_npz("state", optimizers.SGD())
+    # serializers.load_npz("model", model)
+    # opt = serializers.load_npz("state", optimizers.SGD())
 
     data = []
     for source_sentence in text_generator(args.source):
@@ -101,25 +107,24 @@ def test(args, model):
 
 def forward(model, source_sentence, target_sentence, training):
 
-    # convert word to ID, add <End of Sentence>
+    # convert word to ID, add End of Sentence
     source = model.src_vocabulary.convert(source_sentence)
     if target_sentence:
         target = model.trg_vocabulary.convert(target_sentence)
 
-    # hidden state
-    c = Variable(np.zeros((1, model.hidden_size), dtype=np.float32))  # hidden state
+    c = Variable(np.zeros((1, model.hidden_size), dtype=np.float32))
     p = Variable(np.zeros((1, model.hidden_size), dtype=np.float32))
 
     # encoder
     for word_id in source[::-1]:
-        x = Variable(np.array(word_id, dtype=np.int32))  #input one-hot vector
-        e = model.w_xe(x)   # 単語ベクトル
-        p1 = model.w_ep(e)           # hidden layer input
+        x = Variable(np.array(word_id, dtype=np.int32))
+        e = model.w_xe(x)
+        p1 = model.w_ep(e)
         p2 = model.w_pp(p)
 
-        # TODO: W * h
+        # ( W*x + W*h )
         lstm_input = p1 + p2
-        # array ( W * x + W * h ) sigmoid のなかみ
+        # メモリセルと隠れ層の更新
         c, p = F.lstm(c, lstm_input)
 
     # decoder
@@ -130,47 +135,47 @@ def forward(model, source_sentence, target_sentence, training):
     if training:
         loss = Variable(np.zeros((), dtype=np.float32))
         for word_id in target:
-            e = model.w_ye(y)
-            lstm_input = model.w_qq(q) + model.w_eq(e)
+            e = model.w_ey(y)
+            lstm_input = model.w_qq(q) + model.w_qe(e)
             c, q = F.lstm(c, lstm_input)
-            y = model.w_qy(q)
+            y = model.w_yq(q)
             t = Variable(np.array(word_id, dtype=np.int32))
             loss += F.softmax_cross_entropy(y, t)
             y = t
-        #print(loss.data)
         return loss
 
     else:
         sentence = []
         while len(sentence) < 100:
-            e = model.w_ye(y)
-            lstm_input = model.w_qq(q) + model.w_eq(e)
+            e = model.w_ey(y)
+            lstm_input = model.w_qq(q) + model.w_qe(e)
             c, q = F.lstm(c, lstm_input)
-            y = model.w_qy(q)
+            y = model.w_yq(q)
             word_id = np.argmax(y.data, axis=1)
             y = Variable(np.array(word_id, dtype=np.int32))
             if word_id[0] == EOS:
                 sentence.append(model.trg_vocabulary.id_to_word(word_id[0]))
                 break
             sentence.append(model.trg_vocabulary.id_to_word(word_id[0]))
-
-        print(sentence)
         return sentence
 
 
 def main():
     args = parse_args()
-    model = EncoderDecoder(args, args.source, args.target)
 
     if args.mode == "train":
-        # 引数からモデルを定義
+        model = EncoderDecoder(args, args.source, args.target)
+        with open("train_data", "w") as f:
+            f.write(args.source + "\n")
+            f.write(args.target)
         train(args, model)
     else:
         # TODO : load args
+        train_source, train_target = load_train_vocabulary()
+        model = EncoderDecoder(args, train_source, train_target)
         serializers.load_npz("model", model)
         test(args, model)
 
 
 if __name__ == '__main__':
     main()
-
